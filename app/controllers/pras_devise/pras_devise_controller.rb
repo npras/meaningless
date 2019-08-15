1f7136424daa32ed5fcff14d4cee6b072264abe8
Prasanna.Natarajan <prasanna@npras.in>
2019-08-15 19:29:44 +0530

    recaptcha: use v2 only.
    v3 is complicated even though we got it working.
    It's easy to re-use the v2 code for other forms too.

--- app/controllers/pras_devise/pras_devise_controller.rb
+++ app/controllers/pras_devise/pras_devise_controller.rb
@@ -3,10 +3,7 @@ module PrasDevise
 
     private def load_recaptcha_secrets
       v2_creds = Rails.application.credentials.recaptcha[:v2]
-      v3_creds = Rails.application.credentials.recaptcha[:v3]
-
       @v2_site_key, @v2_secret_key = v2_creds.values_at(:site_key, :secret_key)
-      @v3_site_key, @v3_secret_key = v3_creds.values_at(:site_key, :secret_key)
     end
 
   end
--- app/controllers/pras_devise/sessions_controller.rb
+++ app/controllers/pras_devise/sessions_controller.rb
@@ -1,28 +1,19 @@
 module PrasDevise
   class SessionsController < PrasDeviseController
 
+    before_action :set_user, only: :create
     before_action :load_recaptcha_secrets, only: %i(new create)
+    before_action :check_captcha, only: :create
 
     def new
     end
 
     def create
-      @user = User.find_by(email: params[:email])
-
-      recaptcha_success_v3 = verify_recaptcha(site_key: @v3_site_key, secret_key: @v3_secret_key, model: @user, action: :login)
-      recaptcha_success_v2 = verify_recaptcha(site_key: @v2_site_key, secret_key: @v2_secret_key) unless recaptcha_success_v3
-
-      if recaptcha_success_v3 || recaptcha_success_v2
-        if @user&.authenticate(params[:password])
-          login!
-          redirect_to after_sign_in_path_for(:user), notice: "Logged in!"
-        else
-          flash.now.alert = "Email or password is invalid"
-          render :new
-        end
+      if @user&.authenticate(params[:password])
+        login!
+        redirect_to after_sign_in_path_for(:user), notice: "Logged in!"
       else
-        @show_checkbox_recaptcha = true unless recaptcha_success_v3
-        flash.now.alert = "I suspect you are a BOT!"
+        flash.now.alert = "Email or password is invalid"
         render :new
       end
     end
@@ -39,6 +30,17 @@ module PrasDevise
       else
         cookies.encrypted[:remember_token] = @user.remember_token
       end
+    end
+
+    private def set_user
+      @user = User.find_by(email: params[:email])
+    end
+
+    private def check_captcha
+      @recaptcha_success_v2 = verify_recaptcha(site_key: @v2_site_key,
+                                               secret_key: @v2_secret_key)
+
+      render :new and return unless @recaptcha_success_v2
     end
 
   end
