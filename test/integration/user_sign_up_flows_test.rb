f3b0b876181b6598c357e54f9a1c2bb889135e08
Prasanna.Natarajan <prasanna@npras.in>
2019-08-23 11:21:18 +0530

    expired confirmation token test

--- test/integration/user_sign_up_flows_test.rb
+++ test/integration/user_sign_up_flows_test.rb
@@ -33,4 +33,12 @@ class UserSignUpFlowsTest < ActionDispatch::IntegrationTest
     assert_select 'div#notice', "Logged in!"
   end
 
+  test "if confirmation link expires, user is not confirmed" do
+    user = users(:one)
+    user.update(confirmation_token: 'blah', confirmation_sent_at: 1.hours.ago)
+    get "/confirmations/#{user.id}?confirmation_token=#{user.confirmation_token}"
+    assert_and_follow_redirect!
+    assert_response :success
+    assert_select 'div#alert', "Confirmation token has expired. Ask admin to re-send you the confirmation email."
+  end
 end
